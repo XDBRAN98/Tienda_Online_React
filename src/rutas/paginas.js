c898ac29fb0258069140fd1f1f1cf9b0235044fb
Dfsantana1 <122237882+Dfsantana1@users.noreply.github.com>
2023-07-10 06:39:24 -0500

    melos

--- src/rutas/paginas.js
+++ src/rutas/paginas.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
 
-import { Home } from '../paginas/Home/index';
 import LoginForm from '../paginas/auth/login';
 import RegistrationForm from '../paginas/auth/register';
 import { ProductosLista } from "../componentes/Productos/index"
 import { ProductoDetalles } from '../componentes/Productos/ProductoDetalles'
 
+import  Cart  from '../paginas/carrito/cart';
 export const Paginas = () => {
     return (
     <section>
         <Routes>
-            <Route path="/" element={<Home/>}/>
             <Route path="/login" element={<LoginForm/>}/>
             <Route path="/register" element={<RegistrationForm/>}/>
             <Route path="/" element={<ProductosLista/>}/>
             // <Route path="producto/:id" element={<ProductoDetalles/>}/>
 
+            <Route path="/cart" element={ <Cart/>}/>
         </Routes>
     </section>
     )
