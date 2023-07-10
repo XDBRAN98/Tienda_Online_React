25d733936e8650e39b9d8fa3ba1e65cb0828a72c
XDBRAN98 <jhans.velez@utp.edu.co>
2023-07-10 08:56:55 -0500

    solucion iconos carrito y login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 
-
+import 'boxicons';
 
 import { Header } from "./componentes/Header";
 import { Paginas } from "./rutas/paginas"
