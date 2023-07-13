961f7bf32b868b9ddbb0d68ba4d3cf2c5f001144
XDBRAN98 <jhans.velez@utp.edu.co>
2023-07-13 02:13:05 -0500

    cambio url api servidores

--- src/Hooks/UseProductos.js
+++ src/Hooks/UseProductos.js
@@ -1,37 +1,10 @@
 
-/*import { useState, useEffect } from 'react';
-import axios from 'axios';
 
-//const URL = 'https://bootcamp-v13j.onrender.com/products';
-const URL ='http://192.168.20.27:5000/products/';
 
-function useProductos() {
-  const [productos, setProductos] = useState([]);
+const URL = 'https://bootcamp-v13j.onrender.com/products';
+//const URL ='http://192.168.20.27:5000/products/';
 
-  useEffect(() => {
-    const fetchProductos = async () => {
-      try {
-        const response = await axios.get(URL);
-        const data = response.data;
 
-        if (data && Array.isArray(data.productos)) {
-          setProductos(data.productos);
-        } else {
-          console.error('La respuesta de la API no es válida:', data);
-        }
-      } catch (error) {
-        console.error('Error al obtener los productos:', error);
-      }
-    };
-
-    fetchProductos();
-  }, []);
-
-  return { productos };
-}
-
-export { useProductos };
-*/
 
 import { useState, useEffect } from 'react';
 
@@ -41,7 +14,7 @@ function useProductos() {
   useEffect(() => {
     const fetchProductos = async () => {
       try {
-        const response = await fetch('http://192.168.20.27:5000/products');
+        const response = await fetch(URL);
         const data = await response.json();
         setProductos(data.productos);
       } catch (error) {
