9643b93ce3fd80c481c9d61b663c479160b83cf1
Dfsantana1 <122237882+Dfsantana1@users.noreply.github.com>
2023-07-13 13:42:22 -0500

    cf

--- src/guards/guard.js
+++ src/guards/guard.js
@@ -11,4 +11,18 @@ export function autenticacionGuard(Componente) {
       return <Navigate to="/login" />;
     }
   }
+  //guard de admin
+
+  export function adminGuard(Componente) {
+    const accessToken = localStorage.getItem('accessToken');
+    const admin = JSON.parse(localStorage.getItem('user'));
+    
+    if (admin && admin.ID_Rol === 1 && accessToken) {
+      // El usuario está autenticado y es un administrador, permite el acceso al componente
+      return Componente;
+    } else {
+      // Redirige a la página de inicio
+      return <Navigate to="/" replace />;
+    }
+  }
   
