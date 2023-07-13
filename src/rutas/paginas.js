fbf28212c42e1938b46ef08ec65a6c1f4c878de7
XDBRAN98 <jhans.velez@utp.edu.co>
2023-07-13 14:53:51 -0500

    rutas error

--- src/rutas/paginas.js
+++ src/rutas/paginas.js
@@ -1,30 +1,28 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
-import { autenticacionGuard ,adminGuard} from '../guards/guard';
+import { autenticacionGuard, adminGuard } from '../guards/guard';
 
 
-import LoginForm from '../paginas/auth/login';
-import RegistrationForm from '../paginas/auth/register';
-import AdminProfileForm from '../paginas/profile/profile';
-import { ProductosLista } from "../componentes/Productos/index"
-import { ProductoDetalles } from '../componentes/Productos/ProductoDetalles'
+import { LoginForm } from '../paginas/auth/login';
+import { RegistrationForm } from '../paginas/auth/register';
+import { AdminProfileForm } from '../paginas/profile/profile';
+import { ProductosLista } from '../componentes/Productos/index';
+import { ProductoDetalles } from '../componentes/Productos/ProductoDetalles';
+import { Cart } from '../paginas/carrito/cart';
 
-import  Cart  from '../paginas/carrito/cart';
+
 export const Paginas = () => {
     return (
-    <section>
-        <Routes>
-            <Route path="/login" element={<LoginForm/>}/>
-            <Route path="/register" element={<RegistrationForm/>}/>
-            <Route path="/" element={<ProductosLista/>}/>
-            <Route path="/profile" element={adminGuard(<AdminProfileForm />)} />
-            <Route path="producto/:id" element={<ProductoDetalles/>}/>
+        <section>
+            <Routes>
+                <Route path="/login" element={<LoginForm />} />
+                <Route path="/register" element={<RegistrationForm />} />
+                <Route path="/" element={<ProductosLista />} />
+                <Route path="/profile" element={<AdminProfileForm />} />
+                <Route path="producto/:id" element={<ProductoDetalles />} />
+                <Route path="/cart" element={autenticacionGuard(<Cart />)} />
 
-            <Route
-          path="/cart"
-          element={autenticacionGuard(<Cart />)}
-        />
-        </Routes>
-    </section>
+            </Routes>
+        </section>
     )
 }
