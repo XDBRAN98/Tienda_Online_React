5df92a76bacf75d26c13b021c72dbf8c3ea51445
Dfsantana1 <122237882+Dfsantana1@users.noreply.github.com>
2023-07-14 03:10:32 -0500

    god

--- src/componentes/Success/Success.js
+++ src/componentes/Success/Success.js
@@ -3,6 +3,36 @@ import "./Success.css"
 import { GrValidate } from 'react-icons/gr';
 import { BsFillArrowLeftCircleFill } from 'react-icons/bs';
 import { BsFillHandThumbsUpFill } from 'react-icons/bs';
+ 
+
+//funcion que extrae la data del localstorage y la guarda en un objeto a la ruta de la api
+ function postOrder(){
+    const clienteId = JSON.parse(localStorage.getItem('user'));
+    const URL = `http://localhost:5000/success`;
+    const data = {
+        email: clienteId.Email,
+  
+    };
+    console.log(data);
+    localStorage.setItem('order', JSON.stringify(data));
+    //esperar 10 segundos para que se envie la orden
+    fetch(URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+
+        .then(response => response.json())
+        .then(data => {
+            console.log('Orden enviada:', data);
+            // Aquí puedes manejar la respuesta del servidor después de actualizar el producto
+        })
+        .catch(error => {
+            console.error('Error al enviar la orden:', error);
+        });
+}
 
 
 function Success(){
@@ -10,7 +40,7 @@ function Success(){
         <div className="successContainer">
             <div className="Success"><GrValidate className="validate"/></div>
             <h1>Compra realizada correctamente <BsFillHandThumbsUpFill className="hand"/></h1>
-            <a href="/">Volver al inicio <BsFillArrowLeftCircleFill className="return"/></a>
+            <a href="/" onClick={postOrder}> Volver al inicio <BsFillArrowLeftCircleFill className="return"/></a>
         </div>
     )
 }
