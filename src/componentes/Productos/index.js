3ce96bc61af6e5c1304bd3f5d7fdb5f9683946b9
XDBRAN98 <jhans.velez@utp.edu.co>
2023-07-13 20:00:41 -0500

    lo que se hizo en la tarde

--- src/componentes/Productos/index.js
+++ src/componentes/Productos/index.js
@@ -11,59 +11,78 @@ const URL =`${serverBackEndDireccion()}cart/add`;
 
 export const ProductosLista = () => {
 	const { productos } = useProductos();
-
+  
 	const [cartItems, setCartItems] = useState([]);
-	const onAddProduct =  (producto) => {
-		setCartItems([...cartItems, producto]);
-	}
-
-	const enviarCarrito = async () => {
+  
+	const onAddProduct = (producto) => {
+	  setCartItems([...cartItems, producto]);
+	  sendToCart(producto);
+	};
+  
+	const sendToCart = async (producto, clienteId, cantidad) => {
 		try {
+		  const data = {
+			clienteId: clienteId,
+			productoId: producto.ID_Producto,
+			cantidad: cantidad
+		  };
+	  
 		  const response = await fetch(URL, {
 			method: 'POST',
 			headers: {
 			  'Content-Type': 'application/json',
 			},
-			body: JSON.stringify(cartItems),
+			body: JSON.stringify(data),
 		  });
-		  const data = await response.json();
-		  console.log(data);
+	  
+		  const responseData = await response.json();
+		  console.log(responseData);
 		} catch (error) {
 		  console.error('Error al enviar el carrito:', error);
 		}
 	  };
-
-
+	  
 	return (
-		<>
-			<div className="productos">{
-					productos ? (
-						productos.map(producto => (
-							<div className="producto" key={producto.ID_Producto} >
-								<Link to={`/producto/${producto.ID_Producto}`}>
-									<div className="producto__img">
-										<img src={producto.Imagen_1} alt="" />
-									</div>
-								</Link>
-								<div className="producto__footer">
-									<h1>{producto.Nombre_Producto}</h1>
-									<p className="price">${producto.Precio} Cop</p>
-								</div>
-								<div className="buttoms">
-									<button onClick = {() => onAddProduct(producto)} id="btn_añadirCarrito" className="btn">
-										Añadir al carrito
-									</button>
-									<button id="btn_productoDetalles" className="btn2" onClick={() => { window.location.href = `/producto/${producto.ID_Producto}` }}>
-										Vista
-									</button>
-								</div>
-							</div>
-						))
-					) : (
-						<p className="notificacionErrorDataProductos">Cargando productos.</p>
-					)
-				}
-			</div >
-		</>
-	)
-};
+	  <>
+		<div className="productos">
+		  {productos ? (
+			productos.map((producto) => (
+			  <div className="producto" key={producto.ID_Producto}>
+				<Link to={`/producto/${producto.ID_Producto}`}>
+				  <div className="producto__img">
+					<img src={producto.Imagen_1} alt="" />
+				  </div>
+				</Link>
+				<div className="producto__footer">
+				  <h1>{producto.Nombre_Producto}</h1>
+				  <p className="price">${producto.Precio} Cop</p>
+				</div>
+				<div className="buttoms">
+				  <button
+					onClick={() => onAddProduct(producto)}
+					id="btn_añadirCarrito"
+					className="btn"
+				  >
+					Añadir al carrito
+				  </button>
+				  <button
+					id="btn_productoDetalles"
+					className="btn2"
+					onClick={() => {
+					  window.location.href = `/producto/${producto.ID_Producto}`;
+					}}
+				  >
+					Vista
+				  </button>
+				</div>
+			  </div>
+			))
+		  ) : (
+			<p className="notificacionErrorDataProductos">
+			  Cargando productos.
+			</p>
+		  )}
+		</div>
+	  </>
+	);
+  };
